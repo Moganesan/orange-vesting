56c9241b695497b51acbf9fad446333dbd2074b1
moganesan <developer.moganesan@gmail.com>
2024-04-16 17:28:50 +0530

    vesting schedule struct added

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,35 @@ pub struct TokenAllocation {
     pub liquidity: Liquidity,
     pub marketing: u8,
     pub partners: u8,
-    pub stakingReward: u8,
-    pub ecosystemReward: u8,
+    pub staking_reward: u8,
+    pub ecosystem_reward: u8,
     pub airdrop: u8,
-    pub privateSale: u8,
-    pub preSale1: u8,
-    pub preSale2: u8,
-    pub strategicInvestors: u8,
+    pub private_sale: u8,
+    pub pre_sale_1: u8,
+    pub pre_sale_2: u8,
+    pub strategic_investors: u8,
     pub ido: u8,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Liquidity {
-    pub centralizeExchange: u8,
-    pub decentralizedExchange: u8,
+    pub centralize_exchange: u8,
+    pub decentralized_exchange: u8,
+}
+
+pub struct VestingSchedule {
+    pub team: Vec<(u64, u64)>,
+    pub research: Vec<(u64, u64)>,
+    pub marketing: Vec<(u64, u64)>,
+    pub partners: Vec<(u64, u64)>,
+    pub staking_reward: Vec<(u64, u64)>,
+    pub ecosystem_reward: Vec<(u64, u64)>,
+    pub airdrop: Vec<(u64, u64)>,
+    pub private_sale: Vec<(u64, u64)>,
+    pub pre_sale_1: Vec<(u64, u64)>,
+    pub pre_sale_2: Vec<(u64, u64)>,
+    pub strategic_investors: Vec<(u64, u64)>,
+    pub ido: Vec<(u64, u64)>,
 }
 
 // Declare and export the program's entrypoint
